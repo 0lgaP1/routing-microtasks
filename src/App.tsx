e13c036c18b675d557aadeebf54fd9079f02231c
0lgaP1 <volhapatapchuk@gmail.com>
2024-11-16 17:39:15 +0100

    Path object, refactioring

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,17 @@ import {PageTwo} from "./components/pages/PageTwo";
 import {PageThree} from "./components/pages/PageThree";
 import {Navigate, NavLink, Route, Routes} from "react-router-dom";
 import {Error404} from "./components/pages/Error404";
+import styled from "styled-components";
 
 function App() {
+
+    const PATH = {
+        page1: '/page1',
+        page2: '/page2',
+        page3: '/page3',
+        error: '/error404'
+    } as const // чтоб значение свойства в дальнейшем не могло быть переопределено
+
     return (
         <div>
             <div className={styles.header}><h1>HEADER</h1></div>
@@ -17,27 +26,33 @@ function App() {
                                  className={({isActive}: {
                                      isActive: boolean
                                  }) => isActive ? styles.activeNavLink : styles.navLink
-                                 }>Page 1</NavLink></div>
+                                 }>{'Page1'}</NavLink></div>
 
-                    <div><NavLink to={'/page2'}
-                                  className={({isActive}: {
-                                      isActive: boolean
-                                  }) => isActive ? styles.activeNavLink : styles.navLink
-                                  }>Page 2</NavLink></div>
-                    <div><NavLink to={'/page3'}
-                                  className={({isActive}: {
-                                      isActive: boolean
-                                  }) => isActive ? styles.activeNavLink : styles.navLink
-                                  }>Page 3</NavLink></div>
+                    <div>
+                        <NavLink to={'/page2'}
+                                 className={({isActive}: {
+                                     isActive: boolean
+                                 }) => isActive ? styles.activeNavLink : styles.navLink
+                                 }>{'Page2'}</NavLink></div>
+                    <div>
+                        <NavLink to={'/page3'}
+                                 className={({isActive}: {
+                                     isActive: boolean
+                                 }) => isActive ? styles.activeNavLink : styles.navLink
+                                 }>{'Page3'}</NavLink></div>
+
+                    {/*<NavWrapper><NavLink to={'/page1'}>Page 1</NavLink></NavWrapper>*/}
+                    {/*<NavWrapper><NavLink to={'/page2'}>Page 2</NavLink></NavWrapper>*/}
+                    {/*<NavWrapper><NavLink to={'/page3'}>Page 3</NavLink></NavWrapper>*/}
                 </div>
                 <div className={styles.content}>
                     <Routes>
                         <Route path={'/'} element={<Navigate to={'/page1'}/>}/>
-                        <Route path={'/page1'} element={<PageOne/>}/>
-                        <Route path={'/page2'} element={<PageTwo/>}/>
-                        <Route path={'/page3'} element={<PageThree/>}/>
+                        <Route path={PATH.page1} element={<PageOne/>}/>
+                        <Route path={PATH.page2} element={<PageTwo/>}/>
+                        <Route path={PATH.page3} element={<PageThree/>}/>
 
-                        <Route path={'/error404'} element={<Error404/>}/>
+                        <Route path={PATH.error} element={<Error404/>}/>
                         <Route path={'/*'} element={<Navigate to={"/error404"}/>}/>
                     </Routes>
                 </div>
@@ -48,4 +63,26 @@ function App() {
 }
 
 
+// const NavWrapper = styled.div`
+//     margin-left: 10px;
+//     font-size: 20px;
+//
+//     & > a {
+//         text-decoration: none;
+//         color: #1e3786;
+//     }
+//
+//     & > a:active {
+//         text-decoration: none;
+//         color: #ff0346;
+//     }
+//
+//     & > a:hover {
+//         color: steelblue;
+//     }
+//
+// `
+
 export default App;
+
+
