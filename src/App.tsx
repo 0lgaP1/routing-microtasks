3609db05258505eb918a6acb5aab297a3fc88973
0lgaP1 <volhapatapchuk@gmail.com>
2024-11-16 14:38:23 +0100

    Styles to NavLinks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import styles from "./components/Site.module.css";
 import {PageOne} from "./components/pages/PageOne";
-import {PageThree} from "./components/pages/PageThree";
 import {PageTwo} from "./components/pages/PageTwo";
-import {NavLink, Route} from "react-router-dom";
+import {PageThree} from "./components/pages/PageThree";
+import {Navigate, NavLink, Route, Routes} from "react-router-dom";
 import {Error404} from "./components/pages/Error404";
 
 function App() {
@@ -12,20 +12,33 @@ function App() {
             <div className={styles.header}><h1>HEADER</h1></div>
             <div className={styles.body}>
                 <div className={styles.nav}>
-                    <div><NavLink to={'/page1'}>Page 1</NavLink></div>
-                    <div><NavLink to={'/page2'}>Page 2</NavLink></div>
-                    <div><NavLink to={'/page3'}>Page 3</NavLink></div>
+                    <div>
+                        <NavLink to={'/page1'}
+                                 className={({isActive}: {
+                                     isActive: boolean
+                                 }) => isActive ? styles.activeNavLink : styles.navLink
+                                 }>Page 1</NavLink></div>
+
+                    <div><NavLink to={'/page2'}
+                                  className={({isActive}: {
+                                      isActive: boolean
+                                  }) => isActive ? styles.activeNavLink : styles.navLink
+                                  }>Page 2</NavLink></div>
+                    <div><NavLink to={'/page3'}
+                                  className={({isActive}: {
+                                      isActive: boolean
+                                  }) => isActive ? styles.activeNavLink : styles.navLink
+                                  }>Page 3</NavLink></div>
                 </div>
                 <div className={styles.content}>
                     <Routes>
                         <Route path={'/'} element={<Navigate to={'/page1'}/>}/>
+                        <Route path={'/page1'} element={<PageOne/>}/>
+                        <Route path={'/page2'} element={<PageTwo/>}/>
+                        <Route path={'/page3'} element={<PageThree/>}/>
 
-                        <Route path='/page1' element={<PageOne/>} />
-                        <Route path='/page2' element={<PageTwo/>} />
-                        <Route path='/page3' element={<PageThree/>} />
-
-                        <Route path='/error404' element={<Error404/>} />
-                        <Route path='/*' element={<Navigate to={'/error404'}/>} />
+                        <Route path={'/error404'} element={<Error404/>}/>
+                        <Route path={'/*'} element={<Navigate to={"/error404"}/>}/>
                     </Routes>
                 </div>
             </div>
@@ -33,5 +46,6 @@ function App() {
         </div>
     );
 }
+
 
 export default App;
